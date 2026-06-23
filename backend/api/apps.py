c1ed09286785ce34de0937aa0997cf123db114c2
Ocean-2930 <oceanblue1590@gmail.com>
2026-06-23 17:18:37 +0900

    feat: ready() 제거를 통한 django check 최적화
    Refs: #174

--- backend/api/apps.py
+++ backend/api/apps.py
@@ -3,8 +3,3 @@ from django.apps import AppConfig
 
 class ApiConfig(AppConfig):
     name = 'api'
-
-    def ready(self):
-        from .tasks import initialize_celery_worker_availability
-
-        initialize_celery_worker_availability()
