b936389467c5218f37ab1a2b2dede5cbe5d8ff00
Ocean-2930 <oceanblue1590@gmail.com>
2026-06-30 16:11:01 +0900

    feat: masking 로직 정돈
    Refs: #201

--- backend/common/utils.py
+++ backend/common/utils.py
@@ -12,6 +12,40 @@ def load_env():
         load_dotenv(dotenv_path=env_path, encoding="utf-8")
 
 
+def mask(data: dict, mask_result: dict):
+    buff = data
+
+    for key, values in mask_result.items():
+        if not isinstance(values, list):
+            continue
+
+        mask_key = key.upper()
+        for index, value in enumerate(values, start=1):
+            if not isinstance(value, str) or not value:
+                continue
+            mask_format = f"[{mask_key}_{index}]"
+            buff = recursive_alter(buff, value, mask_format)
+    
+    return buff
+
+
+def unmask(data: dict, mask_result: dict):
+    buff = data
+
+    for key, values in mask_result.items():
+        if not isinstance(values, list):
+            continue
+
+        mask_key = key.upper()
+        for index, value in enumerate(values, start=1):
+            if not isinstance(value, str) or not value:
+                continue
+            mask_format = f"[{mask_key}_{index}]"
+            buff = recursive_alter(buff, mask_format, value)
+
+    return buff
+
+
 def recursive_alter(data, target: str, result: str):
     if isinstance(data, str):
         return data.replace(target, result)
