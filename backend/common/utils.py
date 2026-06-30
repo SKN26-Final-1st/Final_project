51ff6a0313e80c128668bbf09b30fcdbf3b18a45
Ocean-2930 <oceanblue1590@gmail.com>
2026-06-30 14:46:10 +0900

    feat: 마스킹 로직 분리
    Refs: #201

--- backend/common/utils.py
+++ backend/common/utils.py
@@ -1,8 +1,15 @@
-from api.models import (
-    Account,
-    AnalysisReport,
-    CompanyInfo,
-    InterviewQuestion,
-    JobDescription,
-    Resume,
-)
+
+def recursive_alter(data, target: str, result: str):
+    if isinstance(data, str):
+        return data.replace(target, result)
+
+    if isinstance(data, list):
+        return [recursive_alter(item, target, result) for item in data]
+
+    if isinstance(data, dict):
+        return {
+            key: recursive_alter(value, target, result)
+            for key, value in data.items()
+        }
+
+    return data
